da4a2551d7ba71905ab9951445ae045d4ad9484f
Scully <51968381+Scullyy@users.noreply.github.com>
2024-01-31 15:39:01 -0800

    Update
    Update to faster webkit exploit

--- ps4900/int64.js
+++ ps4900/int64.js
@@ -4,80 +4,90 @@ function int64(low, hi) {
 
     this.add32inplace = function (val) {
         var new_lo = (((this.low >>> 0) + val) & 0xFFFFFFFF) >>> 0;
-        var new_hi = this.hi;
+        var new_hi = (this.hi >>> 0);
 
         if (new_lo < this.low) {
-            new_hi = (new_hi + 1) & 0xFFFFFFFF;
+            new_hi++;
         }
 
         this.hi = new_hi;
         this.low = new_lo;
-    };
+    }
 
     this.add32 = function (val) {
         var new_lo = (((this.low >>> 0) + val) & 0xFFFFFFFF) >>> 0;
-        var new_hi = this.hi;
+        var new_hi = (this.hi >>> 0);
 
         if (new_lo < this.low) {
-            new_hi = (new_hi + 1) & 0xFFFFFFFF;
+            new_hi++;
         }
 
         return new int64(new_lo, new_hi);
-    };
+    }
 
     this.sub32 = function (val) {
         var new_lo = (((this.low >>> 0) - val) & 0xFFFFFFFF) >>> 0;
-        var new_hi = this.hi;
+        var new_hi = (this.hi >>> 0);
 
-        if (new_lo > this.low) {
-            new_hi = (new_hi - 1) & 0xFFFFFFFF;
+        if (new_lo > (this.low) & 0xFFFFFFFF) {
+            new_hi--;
         }
 
         return new int64(new_lo, new_hi);
-    };
+    }
 
     this.sub32inplace = function (val) {
         var new_lo = (((this.low >>> 0) - val) & 0xFFFFFFFF) >>> 0;
-        var new_hi = this.hi;
+        var new_hi = (this.hi >>> 0);
 
-        if (new_lo > this.low) {
-            new_hi = (new_hi - 1) & 0xFFFFFFFF;
+        if (new_lo > (this.low) & 0xFFFFFFFF) {
+            new_hi--;
         }
 
         this.hi = new_hi;
         this.low = new_lo;
-    };
+    }
 
     this.and32 = function (val) {
         var new_lo = this.low & val;
         var new_hi = this.hi;
         return new int64(new_lo, new_hi);
-    };
+    }
 
     this.and64 = function (vallo, valhi) {
         var new_lo = this.low & vallo;
         var new_hi = this.hi & valhi;
         return new int64(new_lo, new_hi);
-    };
+    }
 
     this.toString = function (val) {
-        val = val || 16;
+        val = 16;
         var lo_str = (this.low >>> 0).toString(val);
         var hi_str = (this.hi >>> 0).toString(val);
-    
-        if (this.hi == 0) {
+
+        if (this.hi == 0)
             return lo_str;
-        } else {
-            lo_str = zeroFill(lo_str, 8);
-            return hi_str + lo_str;
-        }
-    };    
+        else
+            lo_str = zeroFill(lo_str, 8)
+
+        return hi_str + lo_str;
+    }
 
     return this;
 }
 
 function zeroFill(number, width) {
-    width = Math.max(0, width - number.toString().length); // Ensure width is non-negative
+    width -= number.toString().length;
+
+    if (width > 0) {
+        return new Array(width + (/\./.test(number) ? 2 : 1)).join('0') + number;
+    }
+
+    return number + ""; // always return a string
+}
+
+function zeroFill(number, width) {
+    width -= number.toString().length;
 
     if (width > 0) {
         return new Array(width + (/\./.test(number) ? 2 : 1)).join('0') + number;
